511e2206ad563a8979b3b3e8fe9c4d7710bc0c1c
Erick Rodriguez <fenixazul96@hotmail.como>
2019-07-12 17:21:47 -0600

    hola

--- src/proyecto_final/Proyecto_Final.java
+++ src/proyecto_final/Proyecto_Final.java
@@ -15,7 +15,7 @@ public class Proyecto_Final {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("hola");
     }
     
 }
